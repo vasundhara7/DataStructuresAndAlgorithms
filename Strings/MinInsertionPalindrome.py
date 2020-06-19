843f66377a848e509ba61341b357e7302d14d5eb
vasundhara <vasundhara764@gmail.com>
2020-06-19 12:43:37 +0530

    dp solution added

--- Strings/MinInsertionPalindrome.py
+++ Strings/MinInsertionPalindrome.py
@@ -1,4 +1,4 @@
-def mininsertion(s,l,h):
+def mininsertionRecursive(s,l,h):
     if l>h:
         return 100000000
     if l==h:
@@ -14,7 +14,22 @@ def mininsertion(s,l,h):
     else:
         return 1+ min(mininsertion(s,l+1,h),mininsertion(s,l,h-1))
 
+def mininsertionDP(s):
+    
+    dp=[[0 for i in range(len(s))]for j in range(len(s))]
+
+    for gap in range(1,len(s)):
+        l=0
+        for h in range(gap,len(s)):
+            if s[l]==s[h]:
+                dp[l][h]=dp[l+1][h-1]
+            else:
+                dp[l][h]=1+min(dp[l+1][h],dp[l][h-1])
+            l+=1
+    return dp[0][len(s)-1]
+
+
 
 s=input()
-res=mininsertion(s,0,len(s)-1)
+res=mininsertionDP(s)
 print(res)
